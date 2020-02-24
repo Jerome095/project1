a9393bbfc0f872da156fa034272942a4487a0f19
jerome095 <jerome22095@gmail.com>
2020-02-24 20:55:12 +0530

    code is done

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,9 +1,18 @@
 
 public class GitConflicts {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void empId() {
+		
+		System.out.println("6846456");
 	}
+	
+	public void empRname() {
+		System.out.println("jeromejerson");
+	}
+	public void empPhn() {
+		System.out.println("6148948948");
+	}
+	
+
 
 }
